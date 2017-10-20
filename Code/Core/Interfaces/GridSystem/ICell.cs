98f710558291a810ce59ce22c634c1c1388af1d1
Brago <bragolab@gmail.com>
2017-10-20 16:49:36 +0200

    - Added interfaces to GridSystem.

---
+++ Code/Core/Interfaces/GridSystem/ICell.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace ModularFramework.GridSystem {
+
+    public interface ICellModel {
+        Position2D GridPosition { get; }
+    }
+
+    public interface ICellView {
+        RectTransform RectTransform { get; }
+        IGridView Grid { get; set; }
+    }
+
+    #region IGrid interface Extensions
+
+    public static class IGridExtensions {
+
+        public static void MoveTo(this ICellView _this, Position2D _position, bool _allowOutOfBound = false) {
+            _this.RectTransform.DOLocalMove(_this.Grid.GetWorldPositionFromGrid(_position, _allowOutOfBound),
+                        0.08f
+                        ).SetSpeedBased(false);
+        }
+    }
+
+    #endregion
+
+}
---
+++ Code/Core/Interfaces/GridSystem/IGrid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ModularFramework.GridSystem {
+
+    public interface IGridModel {
+
+    }
+
+    public interface IGridView {
+        Vector2 GetWorldPositionFromGrid(Position2D _position, bool _allowOutOfBound);
+    }
+
+
+
+}
