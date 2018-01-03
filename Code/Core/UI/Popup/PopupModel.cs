d53e16f6f2b29e2decf27e7406d98a082e53ecfd
Brago <brago@indieconstruction.com>
2018-01-03 19:54:47 +0100

    - Added PopupModel constructor.

--- Code/Core/UI/Popup/PopupModel.cs
+++ Code/Core/UI/Popup/PopupModel.cs
@@ -12,5 +12,19 @@ namespace ModularFramework.Core {
         public bool Modal;
         public float AutoHideTime;
 
+        /// <summary>
+        /// Create new PopupModel.
+        /// </summary>
+        /// <param name="_title"></param>
+        /// <param name="_text"></param>
+        /// <param name="_modal"></param>
+        /// <param name="_autoHideTime"></param>
+        public PopupModel(string _title, string _text, bool _modal, float _autoHideTime) {
+            Title = _title;
+            Text = _text;
+            Modal = _modal;
+            AutoHideTime = _autoHideTime;
+        }
     }
+
 }
